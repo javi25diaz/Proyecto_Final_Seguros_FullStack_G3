b65ac0c5caaab0628353309063c36a3f9c649e19
javi25diaz <kike25diaz@gmail.com>
2026-07-24 00:41:36 -0500

    point frontend to stable backend production domain

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://proyecto-final-seguros-full-stack-g3-q6o0vo62n.vercel.app/api'
+  apiUrl: 'https://proyecto-final-seguros-full-stack-g.vercel.app/api'
 };
